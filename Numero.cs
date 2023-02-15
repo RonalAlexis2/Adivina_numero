e1b8d3c4b54d174868ed53c81b7808c852d0371b
Ronal Alexis Vargas Moreno <rvargas@ingeneo.local>
2023-02-15 11:13:22 -0500

    ejercicio logica

---
+++ Numero.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adivina_Numero
+{
+    public class Numero
+    {
+        //declaramos la variable de tipo victor, esto siempre debe ser privado por seguridad.
+        public int[] valor { get; set; }
+
+        //inicializamos en cero el vector
+        public Numero()
+        {
+            valor = new int[5] { 0, 0, 0, 0, 0 };
+        }
+        
+        //creamos un metodo llenar de tipo boolean y lo inicializamos en false
+        public bool Llenar(int num)
+        {
+            bool res = false;
+            int i = 4, j = 0;
+
+            //con el try controlamos las exepciones que presente el programa
+            try
+            {
+                //ciclo mientras con logica para ordenar los numeros ingresados por el usuario
+                while (num != 0)
+                {
+                    valor[i] = num % 10;
+                    num /= 10;
+                    i--;
+                }
+                //validamos que el vector en la posicion 0 sea diferente a 0
+                if (valor[0] != 0)
+                {
+                    //recorremos el vector para compararlo con su siguiente posicion
+                    for (i = 0; i < valor.Length; i++)
+                    {
+                        for (j = i + 1; j < valor.Length; j++)
+                        {
+                            //evaluamos que el valor dle vector no se repita
+                            if (valor[i] == valor[j])
+                            {
+                                res = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    res = true;
+                }
+            }
+          // esta es la excepcion del try donde podemos usarla a favor y le damos valor
+            catch (Exception)
+            {
+                res = true;
+            }
+
+            return res;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adivina_Numero
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //instanciamos la clase Numero 
+            Numero numA = new Numero();
+            Numero numB = new Numero();
+
+            //quemamos el valor que queremos que adivine el usuario
+            numA.Llenar(84632);
+
+            //ciclo hacer mientras 
+            do
+            {
+                do
+                { //solicitamos al usuario ingresar los numero
+                    Console.WriteLine("");
+                    Console.WriteLine("Ingrese el numero");
+
+                } // solicitamos llenar siempre que este en true
+                while (numB.Llenar(int.Parse(Console.ReadLine())) == true);
+
+            }//mientras validar sea diferente de 5, indica que ingresaron 5 veces el numero correcto
+            while (validar(numA, numB) != 5);
+
+            Console.WriteLine("Haz ganado");
+
+            Console.ReadKey();
+        }
+
+        //creamos metodo validar en el cual se evaluara si el numero ingresado es correcto o no
+        static int validar(Numero _numA, Numero _numB)
+        {
+            //declaramos variables y las inicializamos
+            int ban = 0, i = 0, j = 0;
+
+            // recorremos los vectores para compararlos y ponerles el signo segun la logica
+            for (i = 0; i < 5; i++)
+            {
+                if (_numA.valor[i] == _numB.valor[i])
+                {
+                    Console.WriteLine(_numA.valor[i] + "(+)");
+                    ban++;
+                }
+                else
+                {
+                    for (j = 0; j < 5; j++)
+                    {
+                        if (_numA.valor[i] == _numB.valor[j])
+                        {
+                            Console.WriteLine(_numA.valor[i] + "(-)");
+                        }
+                    }
+                }
+            }
+
+            return ban;
+        }
+    }
+}
